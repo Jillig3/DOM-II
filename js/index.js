55cbc35700d5121f0dc0addb710e5f002eecd2c3
unknown <jacobillig@gmail.com>
2021-08-31 16:56:07 -0500

    some functionality

--- js/index.js
+++ js/index.js
@@ -11,7 +11,52 @@ const island = document.querySelector('.content-pick .destination:nth-of-type(3)
 const sunBut = document.querySelector('.content-pick .destination .btn');
 const mountBut = document.querySelector('.content-pick .destination:nth-of-type(2) .btn');
 const islandBut = document.querySelector('.content-pick .destination:nth-of-type(3) .btn');
+const nav = document.querySelectorAll('.main-navigation .nav-container .nav');
+
+            // Colored Borders on keystroke Q or E
+
+document.addEventListener('keydown', event => {
+    if (event.keyCode === 81){
+        funBus.style.borderColor = 'red'
+        funBus.style.borderWidth = '10px'
+        funBus.style.borderStyle = 'solid'
+    };
+})
+
+document.addEventListener('keyup', event => {
+    if (event.keyCode === 81){
+        funBus.style.borderColor = 'white'
+    };
+})
+
+document.addEventListener('keydown', event => {
+    if (event.keyCode === 69){
+        letsGo.style.borderColor = 'blue'
+        letsGo.style.borderWidth = '10px'
+        letsGo.style.borderStyle = 'dotted'
+    };
+})
+
+document.addEventListener('keyup', event => {
+    if (event.keyCode === 69){
+        letsGo.style.borderColor = 'white'
+    };
+})
 
 
+            //text color on mouse over
 
+advAwaits.addEventListener('mouseenter', event => {
+    event.target.style.color = 'purple';
+})
 
+dest.addEventListener('mouseenter', event => {
+    event.target.style.color = 'green';
+})
+
+            //nav perm color change
+
+const changeColor = document.querySelectorAll('nav a');
+for (let i = 0; i < changeColor.length; i++){
+  changeColor[i].style.color = 'orange';
+}
