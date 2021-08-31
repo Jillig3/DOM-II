a4250ead9af1e1e37ef379cb5894d1f6e09edaf2
unknown <jacobillig@gmail.com>
2021-08-31 15:53:17 -0500

    created selectors

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+
+            //Selectors
+const funBus = document.querySelector('.intro h2');
+const letsGo = document.querySelector('.content-section .text-content h2');
+const advAwaits = document.querySelector('.inverse-content .text-content h2');
+const dest = document.querySelector('.content-destination h2');
+const sun = document.querySelector('.content-pick .destination h4');
+const mount = document.querySelector('.content-pick .destination:nth-of-type(2) h4');
+const island = document.querySelector('.content-pick .destination:nth-of-type(3) h4');
+const sunBut = document.querySelector('.content-pick .destination .btn');
+const mountBut = document.querySelector('.content-pick .destination:nth-of-type(2) .btn');
+const islandBut = document.querySelector('.content-pick .destination:nth-of-type(3) .btn');
+
+
+
+
